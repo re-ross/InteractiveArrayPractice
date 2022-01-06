52e1413986a039c7105615ef54211f2538f7c5f7
reross <ryan@reross.com>
2022-01-06 12:43:31 -0600

    interactive array practice

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log(`hello world`);
+
+let backpack = [];
+
+backpack.push("sword", "potion");
+backpack.push("food");
+backpack.push("shield");
+
+// let onBelt = backpack.splice(0,1)
+let onBelt = backpack.shift();
+
+// console.log("Backpack: ", backpack, "Belt: ", onBelt);
+
+let furCoat = "fur coat";
+
+backpack.push(furCoat);
+
+backpack.pop();
+
+let itemCount = backpack.length;
+
+backpack.push("flint", "blanket", "knife", "toothbrush");
+
+let backpack2 = backpack.splice(3, 3);
+
+// console.log(backpack2[1]);
+
+// console.log(
+//   "Backpack2: ",
+//   backpack2,
+//   "Belt: ",
+//   onBelt,
+//   "item-count: ",
+//   itemCount
+// );
+for (let i = 0; i < 3; i++) {
+  console.log(backpack[i]);
+}
+
+console.log("----------");
+for (let i = 0; i < backpack2.length; i++) {
+  console.log(backpack2[i]);
+}
